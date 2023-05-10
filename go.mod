5cbd16675b6b29b0cc3b574edb0cc75a4638ec50
fsidiqs <fajarsidiqsalviro@gmail.com>
2023-05-10 14:25:35 +0700

    developing server_test.go

---
+++ go.mod
@@ -0,0 +1,25 @@
+module github.com/fsidiqs/proglog
+
+go 1.20
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/kr/pretty v0.3.1
+	github.com/stretchr/testify v1.8.2
+	github.com/tysonmote/gommap v0.0.2
+	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1
+	google.golang.org/grpc v1.55.0
+	google.golang.org/protobuf v1.30.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.9.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ internal/server/server_test.go
@@ -0,0 +1,78 @@
+package server
+
+import (
+	"context"
+	"io/ioutil"
+	"net"
+	"testing"
+
+	api "github.com/fsidiqs/proglog/api/v1"
+	"github.com/fsidiqs/proglog/internal/log"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+)
+
+func TestServer(t *testing.T) {
+	for scenario, fn := range map[string]func(
+		t *testing.T,
+		client api.LogClient,
+		config *Config,
+	){
+		"produce/consume a message to/from the log succeeds": testProduceConsume,
+		"produce/consume stram succeeds":                     testProduceConsumeStream,
+		"consume past log boundary fails":                    testConsumePastBoundary,
+	} {
+		t.Run(scenario, func(t *testing.T) {
+			client, config, teardown := setupTest(t, nil)
+			defer teardown()
+			fn(t, client, config)
+		})
+	}
+}
+
+func setupTest(t *testing.T, fn func(*Config)) (
+	client api.LogClient,
+	cfg *Config,
+	teardown func(),
+) {
+	t.Helper()
+	l, err := net.Listen("tcp", ":0")
+	require.NoError(t, err)
+
+	clientOptions := []grpc.DialOption{grpc.WithInsecure()}
+	cc, err := grpc.Dial(l.Addr().String(), clientOptions)
+	require.NoError(t, err)
+
+	dir, err := ioutil.TempDir("", "server-test")
+	require.NoError(t, err)
+
+	clog, err := log.NewLog(dir, log.Config{})
+	require.NoError(t, err)
+
+	cfg = &Config{
+		CommitLog: clog,
+	}
+
+	if fn != nil {
+		fn(cfg)
+	}
+
+	server, err := NewGRPCServer(cfg)
+	require.NoError(t, err)
+
+	go func() {
+		server.Serve(l)
+	}()
+
+	client = api.NewLogClient(cc)
+	return client, cfg, func() {
+		server.Stop()
+		cc.Close()
+		l.Close()
+		clog.Remove()
+	}
+}
+
+func testProduceConsume(t *require.TestingT, client api.LogClient, config *Config) {
+	ctx := context.Background()
+}
